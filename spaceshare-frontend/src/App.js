5db4ff5498cc533979e1c19f3c7e0d16933c6d02
praveenkumarreddyburla <77834549+praveenkumarreddyburla@users.noreply.github.com>
2024-08-02 23:26:54 -0300

    Update App.js

--- spaceshare-frontend/src/App.js
+++ spaceshare-frontend/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import React from "react";
 import Home from "./views/home/home";
 import Navbar from "./components/navbar/navbar";
@@ -11,6 +10,8 @@ import Faq from "./views/faq/faq";
 import Footer from "./components/footer/footer";
 import Dashboard from "./views/dashboard/dashboard";
 import RequireAuth from "./utils/RequireAuth";
+import Results from "./views/result/results";
+import History from "./views/history/history";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Contact from "./views/contact/contact";
 
@@ -26,24 +27,21 @@ function App() {
         <Navbar />
         <Routes>
           {/* public routes */}
-          <Route path="/" element={<Home />} /> 
+          <Route path="/" element={<Home />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password/:token" element={<ResetPassword />} />
           <Route path="/contact" element={<Contact />} />
-
+          <Route path="/search" element={<Results />} />{" "}
+          {/* Ensure Results route is set */}
           {/* protect routes */}
-          {/* user routes */}
-          <Route element={<RequireAuth allowedRoles={[ROLES.USER]} />}></Route>
-          {/* space owner route */}
-          <Route
-            element={<RequireAuth allowedRoles={[ROLES.SPACE_OWNER]} />}
-          ></Route>
+          <Route element={<RequireAuth allowedRoles={[ROLES.USER]} />} />
+          <Route path="/history" element={<History />} />
+          <Route element={<RequireAuth allowedRoles={[ROLES.SPACE_OWNER]} />} />
           <Route element={<RequireAuth allowedRoles={[ROLES.SPACE_OWNER]} />}>
             <Route path="/dashboard" element={<Dashboard />} />
           </Route>
-          {/* common routes */}
           <Route
             element={
               <RequireAuth allowedRoles={[ROLES.USER, ROLES.SPACE_OWNER]} />
